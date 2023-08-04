d463d7d4af5662c7745e3eb41b42b2b119290a67
SujeetYT <sujeet7982422191@gmail.com>
2023-08-04 14:56:07 +0530

    Fixed API Key exposed

--- Backend/index.js
+++ Backend/index.js
@@ -25,44 +25,41 @@ app.post('/flightfare', async (req, res)=>{
         currency: `${currency}`
       },
       headers: {
-        'X-RapidAPI-Key': 'fa7e9ac395msh65c36b5c78adb7bp1404adjsn8b960f2d312a',
+        'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
         'X-RapidAPI-Host': 'flight-fare-search.p.rapidapi.com'
       }
     };
     
     try {
-        // const response = await axios.request(options);
-        const response = dummyData;
+        const response = await axios.request(options);
+        // const response = dummyData;
         
         // sending desired data
         const newData = [];
-        // response.data.results.map((data)=>{
-        //     const obj = {
-        //         "flight_name":`${data.flight_name}`, // Air-India
-        //         "careerCode": `${data.careerCode}`,  // AI
-        //         "cabinType": `${cabinType}`,         // Economy
-        //         "currency": `${currency}`,           // INR
-        //         "total":`${data.totals.total}`,      // Fare price
-        //     };
-        //     newData.push(obj);
-        // });
+        response[0].results.map((data)=>{
+          const obj = {
+            "flight_name": data.flight_name, // Air-India
+            "careerCode": data.careerCode,   // AI
+            "cabinType": data.cabinType,     // Economy
+            "currency": data.currency,       // INR
+            "total": data.totals.total,      // Fare price
+          };
+          newData.push(obj);
+        });
 
-        // ----------------------------------------------------------------
-        // response[0].results.map((data)=>{
-        //     const obj = {
-        //         "flight_name":`${data.flight_name}`, // Air-India
-        //         "careerCode": `${data.careerCode}`,  // AI
-        //         "cabinType": `${cabinType}`,         // Economy
-        //         "currency": `${currency}`,           // INR
-        //         "total":`${data.totals.total}`,      // Fare price
-        //     };
-        //     newData.push(obj);
+        // ------------------------- Mock data starts ---------------------------------------
+        // response.map((data)=>{
+        //   const obj = {
+        //     "flight_name": data.flight_name, // Air-India
+        //     "careerCode": data.careerCode,   // AI
+        //     "cabinType": data.cabinType,     // Economy
+        //     "currency": data.currency,       // INR
+        //     "total": data.totals.total,      // Fare price
+        //   };
+        //   newData.push(obj);
         // });
-        
-        res.send(response[0].results[0]);
-
-        //--------------------------------------------------------------------
-        // res.send(newData);
+        // ------------------------- Mock data ends ---------------------------------------
+        res.send(newData);
 
     } catch (error) {
         res.send(error);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Architecture of the Backend
+
+<div style="background-color: white">
+    <img src="./Architecture.svg" alt="Low Level Design">
+</div>
+
